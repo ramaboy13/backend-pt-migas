23859e3498fa3e2e624d4f7a80a4a93824c6e7fd
Barito Surya Ramadhani <42413899+ramaboy13@users.noreply.github.com>
2025-12-13 23:35:41 +0700

    Update filesystem root paths in configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
+            'root' => storage_path('app'),
             'serve' => true,
             'throw' => false,
             'report' => false,
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('/home/baritos1/backend-mentaya.baritosuryaramadhani.site/storage/app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
