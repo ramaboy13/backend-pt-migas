25b1c268c131233b33c2e6d2948fa3311e7bfc14
baritosuryaramadhani <baritosuryaramadhani@MyMac.local>
2026-02-06 22:26:05 +0700

    fix factory assets

--- database/factories/AssetFactory.php
+++ database/factories/AssetFactory.php
@@ -4,11 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use Illuminate\Support\Str;
+
 class AssetFactory extends Factory
 {
     public function definition(): array
     {
         return [
+            'id'         => (string) Str::uuid(), // ✅ WAJIB
             'nama'       => $this->faker->randomElement([
                 'Laptop',
                 'Monitor',
@@ -29,3 +32,4 @@ class AssetFactory extends Factory
         ];
     }
 }
+
